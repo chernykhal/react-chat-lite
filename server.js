1aa48aace9daae0533feaf5a25e0b6cca7a93ac2
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-18 23:27:37 +0500

    (feat) edit server side

--- server.js
+++ server.js
@@ -3,11 +3,26 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, { cors: { origin: "*" } });
+app.use(express.json());
 
-const rooms = new Map([[], []]);
+const rooms = new Map();
 
 app.get("/rooms", function (req, res) {
   res.json(rooms);
+});
+
+app.post("/rooms", (req, res) => {
+  const { roomId, userName } = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(
+      roomId,
+      new Map([
+        ["users", new Map()],
+        ["messages", []],
+      ])
+    );
+    res.json(rooms);
+  }
 });
 
 io.on("connection", (socket) => {

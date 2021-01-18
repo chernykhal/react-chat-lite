3bb82ab373355655b854015919b3cdc4ba9f2bc9
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-18 21:19:35 +0500

    (feat) add web socket to server side

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 const rooms = new Map([[], []]);
 
-app.get("/rooms", function (req, res) {});
-app.listen(9999, (error) => {
+app.get("/rooms", function (req, res) {
+  res.json(rooms);
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket);
+});
+
+server.listen(9999, (error) => {
   if (error) {
     throw Error(error);
   }
